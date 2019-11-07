4710037024e4a0a2f40d84f1582410b3cc650db3
AlexandreTomasi <alexandretomasi18@gmail.com>
2019-11-07 00:37:36 -0400

    Commite da carga de dados para o banco de dados. Conforme solicitado

---
+++ carga.sql
@@ -0,0 +1,21 @@
+insert into artista(id, nome) values(1, 'Serj tankian');
+insert into artista(id, nome) values(2, 'Mike Shinoda');
+insert into artista(id, nome) values(3, 'Michel Teló');
+insert into artista(id, nome) values(4, 'Guns N Roses');
+
+insert into albuns (id, nome_Albun, artista_id) values (1, 'Harakiri', 1);
+insert into albuns (id, nome_Albun, artista_id) values (2, 'Black Blooms', 1);
+insert into albuns (id, nome_Albun, artista_id) values (3, 'The RoughDog', 1);
+
+insert into albuns (id, nome_Albun, artista_id) values (4, 'The Rising Tied', 2);
+insert into albuns (id, nome_Albun, artista_id) values (5, 'Post Traumatic', 2);
+insert into albuns (id, nome_Albun, artista_id) values (6, 'Post Traumatic EP', 2);
+insert into albuns (id, nome_Albun, artista_id) values (7, 'Where d You Go', 2);
+
+insert into albuns (id, nome_Albun, artista_id) values (8, 'Bem Sertanejo',3);
+insert into albuns (id, nome_Albun, artista_id) values (9, 'Bem Sertanejo - O Show (Ao vivo)',3);
+insert into albuns (id, nome_Albun, artista_id) values (10, 'em Sertanejo - (1º Temporada) - EP',3);
+
+insert into albuns (id, nome_Albun, artista_id) values (11, 'Use Your Illlusion I',4);
+insert into albuns (id, nome_Albun, artista_id) values (12, 'User Your Illlusion II',4);
+insert into albuns (id, nome_Albun, artista_id) values (13, 'Greatest Hits',4);
