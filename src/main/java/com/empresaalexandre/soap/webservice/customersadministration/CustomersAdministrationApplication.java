73b88f0d8a861c92a11b97145a68b89c3159b5bd
AlexandreTomasi <alexandretomasi18@gmail.com>
2019-11-07 01:45:27 -0400

    Commit da classe que contem o main para executar os webservices e repositorios.

---
+++ src/main/java/com/empresaalexandre/soap/webservice/customersadministration/CustomersAdministrationApplication.java
@@ -0,0 +1,19 @@
+package com.empresaalexandre.soap.webservice.customersadministration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.empresaalexandre.soap.webservice.customersadministration.repository.CustomerRepository;
+
+@SpringBootApplication
+@EntityScan(basePackages = {"com.empresaalexandre.soap.webservice.customersadministration.model"})
+@EnableJpaRepositories(basePackages = {"com.empresaalexandre.soap.webservice.customersadministration.repository"})
+public class CustomersAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersAdministrationApplication.class, args);
+		//http://localhost:8080/ws
+	}
+}
